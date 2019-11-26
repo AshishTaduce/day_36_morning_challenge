1c1270db3427a3e4a75bc1d2058b4e1155d6b004
Ashish <ashishteddy2@gmail.com>
2019-11-26 16:50:44 +0530

    done with 2 test cases

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,4 @@
+import 'dart:core';
 /// Challenge
 /// Accounts Merge
 // Given a list accounts, each element accounts[i] is a list of strings, where
@@ -21,5 +22,61 @@
 /// Output: [["John", 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com'],
 /// ["John", "johnnybravo@mail.com"], ["Mary", "mary@mail.com"]]
 
+class Account {
+  String name;
+  List<String> emails = [];
+
+  Account(this.name, this.emails);
+
+  bool duplicateEmail(Account other) {
+    return other.emails.any((String email) => this.emails.contains(email));
+  }
+
+  Account mergedAccounts(Account other) {
+    this.emails.addAll(other.emails);
+    return Account(this.name, this.emails.toSet().toList());
+  }
+}
+
+dynamic accountsMerge(List<List<String>> accounts) {
+  List<Account> accountsList = [];
+
+  for (int i = 0; i < accounts.length; i++) {
+    List<String> emails = [];
+
+    for (int j = 1; j < accounts[i].length; j++) {
+      emails.add(accounts[i][j]);
+    }
+    accountsList.add(Account(accounts[i][0], emails));
+    emails = [];
+  }
+  // for loop for matching accounts
+
+  for (int i = 0; i < accountsList.length - 1; i++) {
+    for (int j = i + 1; j < accountsList.length; j++) {
+      if (accountsList[i].duplicateEmail(accountsList[j])) {
+        accountsList[i] = accountsList[i].mergedAccounts(accountsList[j]);
+        accountsList.removeAt(j);
+        j = i;
+      }
+    }
+  }
+  List<List<String>> result = [];
+  for (int i = 0; i < accountsList.length; i++) {
+    List<String> temp = [];
+    temp.add(accountsList[i].name);
+    accountsList[i].emails.sort();
+    temp.addAll(accountsList[i].emails);
+    result.add(temp);
+  }
+  return result;
+}
+
 main() {
+  print(accountsMerge([
+    ["John", "johnsmith@mail.com", "john00@mail.com"],
+    ["John", "johnnybravo@mail.com"],
+    ["John", "johnsmith@mail.com", "john_newyork@mail.com"],
+    ["Mary", "mary@mail.com"]
+  ]));
 }
